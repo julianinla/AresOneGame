e73229f7c0fc3357219816b5820f89473c1ece3e
Julian van Riet <julianinla@me.com>
2017-04-02 20:49:02 -0700

    Changed Direction of the Game

---
+++ AresOne/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
+    private float inverseMoveTime;
+
+	protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1.0f / moveTime;
+	}
+
+    protected bool CanObjectMove(int xDirection, int yDirection)
+    {
+        Vector2 startPosition = rigidBody.position;
+        Vector2 endPosition = startPosition + new Vector2(xDirection, yDirection);
+
+        StartCoroutine(SmoothMovementRoutine(endPosition));
+
+        return true;
+    }
+
+    protected IEnumerator SmoothMovementRoutine(Vector2 endPosition)
+    {
+        float remainingDistanceToEndPosition;
+
+        do
+        {
+            remainingDistanceToEndPosition = (rigidBody.position - endPosition).sqrMagnitude;
+            Vector2 updatedPosition = Vector2.MoveTowards(rigidBody.position, endPosition, inverseMoveTime * Time.deltaTime);
+            rigidBody.MovePosition(updatedPosition);
+            yield return null;
+        } while (remainingDistanceToEndPosition > float.Epsilon);
+    } 
+	
+	void Update () {
+	
+	}
+}
---
+++ AresOne/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+    Animator animator;
+
+	void Update () {
+
+        animator = GetComponent<Animator>();
+
+        int xAxis = 0;
+        int yAxis = 0;
+        bool attack = false;
+
+        xAxis = (int) Input.GetAxisRaw("Horizontal");
+        yAxis = (int) Input.GetAxisRaw("Vertical");
+        attack = Input.GetKeyDown("z");
+
+        animator.SetBool("playerAttack", attack);
+
+        if (xAxis != 0)
+        {
+            yAxis = 0;
+        }
+
+        if (xAxis != 0 || yAxis != 0)
+        {
+            CanObjectMove(xAxis, yAxis);
+        }
+
+        if (yAxis > 0)
+        {
+            animator.SetBool("moveNorth", true);
+        }
+        else if (yAxis < 0)
+        {
+            animator.SetBool("moveSouth", true);
+        }
+        else if (xAxis < 0)
+        {
+            animator.SetBool("moveWest", true);
+        }
+        else if (xAxis > 0)
+        {
+            animator.SetBool("moveEast", true);
+        }
+    }
+}
