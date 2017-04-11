117cbf94b2dc3052b052f9adb1f5ef76df02446b
Julian van Riet <julianinla@me.com>
2017-04-10 22:44:55 -0700

    Reload First Level When Player Dies

---
+++ AresOne/Assets/Scripts/GameContoller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameContoller : MonoBehaviour {
+
+    public static GameContoller Instance;
+
+	void Awake () {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	void Update () {
+	
+	}
+}
--- AresOne/Assets/Scripts/Player.cs
+++ AresOne/Assets/Scripts/Player.cs
@@ -6,23 +6,12 @@ public class Player : MovingObject {
     Animator animator;
     private float speed = 4f;
     private float vertSpeed = 5f;
+    private int secondUntilLevelLoads;
 
     void Update () {
 
         animator = GetComponent<Animator>();
 
-        /* int xAxis = 0;
-        int yAxis = 0;
-
-        xAxis = (int) Input.GetAxisRaw("Horizontal");
-        Debug.Log("xAxis" + xAxis);
-        yAxis = (int) Input.GetAxisRaw("Vertical");
-        Debug.Log("yAxis" + yAxis);
-
-        if (xAxis != 0 || yAxis != 0)
-        {
-            CanObjectMove(xAxis, yAxis);
-        }*/
         animator.SetBool("isRunning", false);
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -39,5 +28,29 @@ public class Player : MovingObject {
         if(Input.GetKey(KeyCode.UpArrow)) {
             transform.Translate(Vector2.up * vertSpeed * Time.deltaTime);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D objectPlayerCollidedWith)
+    {
+        if (objectPlayerCollidedWith.tag == "Spike")
+        {
+            Debug.Log("Hit Spike");
+            Invoke("onDeath", secondUntilLevelLoads);
+        }
+        else if (objectPlayerCollidedWith.tag == "Exit")
+        {
+            Debug.Log("Hit Exit");
+            //Invoke("LoadNewLevel", secondUntilLevelLoads);
+        }
+    }
+
+    //private void LoadNewLevel()
+    //{
+    //    Application.LoadLevel(Application.loadedLevel + 1);
+    //}
+
+    private void onDeath()
+    {
+        Application.LoadLevel(0);
     }
 }
